f3b188641a79d4753c782fa1065c56d253087074
JagadishBujji <jagadish00198@gmail.com>
2025-06-26 23:16:57 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+import NavHeader from "@/components/layouts/NavHeader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavHeader/>
         {children}
       </body>
     </html>
---
+++ src/components/layouts/NavHeader.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Image from "next/image";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import classes from "../layouts/NavHeader.module.css";
+
+function NavHeader() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">
+          <img src="madhurilogo.svg" alt="Logo"  className={`${classes.logo}`}  />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="m-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#" className={classes.navItem}>Home</Nav.Link>
+            <Nav.Link href="#" className={classes.navItem}>Collections</Nav.Link>
+            <Nav.Link href="#" className={classes.navItem}>About Us</Nav.Link>
+            <Nav.Link href="#" className={classes.navItem}>Our Policies</Nav.Link>
+            <Nav.Link href="#" className={classes.navItemcontact}>Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavHeader;
