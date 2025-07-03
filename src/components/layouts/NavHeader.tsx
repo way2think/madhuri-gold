d1ebe0cb8cc370579f10b0a20a6597bfc251a98e
sanjay-3129 <ksanjay00197@gmail.com>
2025-07-03 13:55:09 +0530

    navbar link

--- src/components/layouts/NavHeader.tsx
+++ src/components/layouts/NavHeader.tsx
@@ -3,14 +3,17 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Link from "next/link";
 import classes from "../layouts/NavHeader.module.css";
 
 function NavHeader() {
   return (
     <Navbar expand="lg" className={`${classes.navbar} bg-body-tertiary`}>
       <Container>
-        <Navbar.Brand href="/">
-          <img src="madhurilogo.svg" alt="Logo" className={`${classes.logo}`} />
+        <Navbar.Brand as="span">
+          <Link href="/" className={classes.logo} passHref>
+            <img src="madhurilogo.svg" alt="Logo" className={classes.logo} />
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -19,21 +22,31 @@ function NavHeader() {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="/" className={classes.navItem}>
-              Home
-            </Nav.Link>
-            <Nav.Link href="#" className={classes.navItem}>
-              Collections
-            </Nav.Link>
-            <Nav.Link href="/about-us" className={classes.navItem}>
-              About Us
-            </Nav.Link>
-            <Nav.Link href="#" className={classes.navItem}>
-              Our Policies
-            </Nav.Link>
-            <Nav.Link href="#" className={classes.navItemcontact}>
-              Contact Us
-            </Nav.Link>
+            <Nav.Item>
+              <Link href="/" className={classes.navItem}>
+                Home
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/collections" className={classes.navItem}>
+                Collections
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/about-us" className={classes.navItem}>
+                About Us
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="#" className={classes.navItem}>
+                Our Policies
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="#" className={classes.navItemcontact}>
+                Contact Us
+              </Link>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
