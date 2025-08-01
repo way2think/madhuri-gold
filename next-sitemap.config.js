bf9beeaa7e6f1cba80c74227b820317b1536f3b5
sanjay-3129 <ksanjay00197@gmail.com>
2025-08-01 22:10:55 +0530

    sitemap,robots.txt added

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://madhurigold.com',
+  generateRobotsTxt: true,
+  outDir: './out',
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
